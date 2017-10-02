020aabd4d5ad41ee7369d5853ea344e4164cb99e
YangyangYi <yi.yan@husky.neu.edu>
2017-10-02 19:00:06 -0400

    lab3

---
+++ Lab3/src/Business/Account.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.Date;
+
+/**
+ *
+ * @author yiyangyang
+ */
+public class Account {
+    private String routingNumber;
+    private String accountNumber;
+    private String bankName;
+    private int balance;
+    private Date createOn;
+
+    public Account() {
+        this.createOn = new Date();
+    }
+
+    public Date getCreateOn() {
+        return createOn;
+    }
+    
+    public String getRoutingNumber() {
+        return routingNumber;
+    }
+
+    public void setRoutingNumber(String routingNumber) {
+        this.routingNumber = routingNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    public String toString(){
+        return this.bankName;
+    }
+}
